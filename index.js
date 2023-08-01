862c892ea105bfecff60788c02db9e85869e17a4
largodav <albarranco2@gmail.com>
2023-08-01 16:39:29 +0200

    anadiendo codigo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //esto es un comentario, de prueba
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
